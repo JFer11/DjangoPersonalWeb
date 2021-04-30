7b3e67a6f031772ba4659cd4447ababf3eee8bba
Joaco <jnfernandez.ojeda@gmail.com>
2021-04-30 17:02:32 -0300

    Inheritance in templates

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,14 +1,4 @@
-from django.shortcuts import render, HttpResponse
-
-
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-    <li> <a href="/"> Portada </a> </li>
-    <li> <a href="/about-me"> Acerca de </a> </li>
-    <li> <a href="/portfolio"> Portfolio </a> </li>
-    <li> <a href="/contact"> Contactame </a> </li>
-"""
+from django.shortcuts import render
 
 
 def home(request):
