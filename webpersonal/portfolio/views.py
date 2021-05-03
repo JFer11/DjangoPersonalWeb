4e59bec091a110f4d3002550f99eb59304afeb60
Joaco <jnfernandez.ojeda@gmail.com>
2021-05-02 22:29:22 -0300

    Dynamically generated html

--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+
+def portfolio(request):
+    """ All projects created through the Admin Page will be dynamically loaded into the response template. """
+    projects = Project.objects.all()
+    return render(request, "portfolio/portfolio.html", {"projects": projects})
